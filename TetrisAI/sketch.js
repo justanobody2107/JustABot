180bb27b01e6f0bc409630471e01125eb14d57b3
JustANobody <42755661+justanobody2107@users.noreply.github.com>
2020-12-17 11:40:56 -0500

    Attempted font fix

--- TetrisAI/sketch.js
+++ TetrisAI/sketch.js
@@ -29,7 +29,7 @@ let populationSize = 160;
 
 
 function preload() {
-    font = loadFont("tetris_font/Square.ttf");
+    font = loadFont("libraries/tetris_font/Square.ttf");
 }
 
 function setup() {
